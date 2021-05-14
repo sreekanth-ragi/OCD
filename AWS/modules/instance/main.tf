234a1b39fe4ddba107b95df4ccfc56cd000b30a5
Sreekanth <ragisreekanth@gmail.com>
2021-05-14 08:12:53 +0530

    Fix Formatting

--- AWS/modules/instance/main.tf
+++ AWS/modules/instance/main.tf
@@ -1,6 +1,6 @@
 resource "tls_private_key" "pvt_key" {
-  algorithm							= "RSA"
-  rsa_bits							= 1024    # Create a "myKey.pem" to your computer
+  		algorithm			= "RSA"
+  		rsa_bits			= 1024    # Create a "myKey.pem" to your computer
 
   provisioner "local-exec" {
     command = "echo '${tls_private_key.pvt_key.private_key_pem}' > ./pvt_key.pem"
@@ -8,23 +8,23 @@ resource "tls_private_key" "pvt_key" {
 }
 
 resource "aws_key_pair" "key_pair" {
-  key_name   = "pvtKey"
-  public_key = tls_private_key.pvt_key.public_key_openssh
+ 		 key_name  			= "pvtKey"
+ 		 public_key 			= tls_private_key.pvt_key.public_key_openssh
 }
 
 
 resource "aws_instance" "instance" {
-	count							= var.instance_count
-	ami								= var.ami_id
-	instance_type					= var.instance_type
-	key_name						= aws_key_pair.key_pair.key_name
+	count					= var.instance_count
+	ami					= var.ami_id
+	instance_type				= var.instance_type
+	key_name				= aws_key_pair.key_pair.key_name
 	vpc_security_group_ids			= var.sg_id
-	subnet_id						= var.is_public ? element(var.pub_net_id, 0): element(var.pvt_net_id, 0)
+	subnet_id				= var.is_public ? element(var.pub_net_id, 0): element(var.pvt_net_id, 0)
 	associate_public_ip_address		= var.is_public ? true : false
-	//user_data						= data.template_cloudinit.user_data.rendered
+	//user_data				= data.template_cloudinit.user_data.rendered
 
-	tags							= {
-											Name		= var.hostname
-											Biz			= var.env
-										}
+	tags					= {
+							Name	= var.hostname
+							Biz	= var.env
+						}
 }
